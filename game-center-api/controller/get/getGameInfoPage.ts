378621eab5fefc7274bf8ade527ee96f33760917
de00x <97359971+de00x@users.noreply.github.com>
2023-01-05 17:56:46 +0300

    created api for game-info-page

---
+++ game-center-api/controller/get/getGameInfoPage.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import { DbModel } from "../../db/connect";
+
+export const getGameInfoPage = async (req: Request, res: Response) => {
+  const currentGameInfoPage = Number(req.query.currentInfoPage);
+
+  const success = await DbModel.getGameInfoPage(currentGameInfoPage);
+  if (success) {
+    res.json(success);
+  } else res.json({ success: false });
+};
